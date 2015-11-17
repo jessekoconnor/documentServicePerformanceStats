60d19720863c9997b1818a10a727ea0adb407584
Jesse O'Connor <jesse.oconnor@meltwater.com>
2015-11-17 16:09:39 -0500

    Better output as well as a better working json doc to send

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function postAndGet(done) {
 
 	// Post the contents of content.json
 	postIt.makePost(function(param) {
-		console.log('-- Doc posted --> ' + param)
+		console.log('-- Doc ' + (runs+1) + ' posted --> ' + param)
 
 		// Post the contents of content.json
 		getIt.makeGet(param, function(tries, error) {
@@ -34,9 +34,9 @@ var runningTotalTime = 0;
 // pretty
 function printSummary() {
 	console.log('-----------------------------------------');
-	console.log('done with all', config.totalRuns, 'runs!');
+	console.log('done with', runs, 'runs!');
 	console.log(unretrievableDocs + ' documents were not retrievable');
-	console.log((config.totalRuns - unretrievableDocs) + ' were retrieved averaging to', (runningTotalTime/(config.totalRuns-unretrievableDocs)));
+	console.log((runs - unretrievableDocs) + ' were retrieved averaging to', (runningTotalTime/(runs-unretrievableDocs)));
 }
 
 // run tests one after the other (synchronously)
@@ -52,3 +52,11 @@ function runTestsSynchronously() {
 
 // Kick things off
 runTestsSynchronously();
+
+
+process.on('uncaughtException', function (err) {
+  console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+  console.error(err.stack)
+  printSummary();
+  process.exit(1)
+})
